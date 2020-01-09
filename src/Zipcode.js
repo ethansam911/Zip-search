9187bb2adbf269783d91a3e90b0054d26608be26
Emily Fang <fangemily60@gmail.com>
2020-01-09 12:15:03 -0500

    Post-review with instructors

--- src/Zipcode.js
+++ src/Zipcode.js
@@ -4,67 +4,73 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 
 class Zipcode extends React.Component {
-constructor(props) {
-    super(props);
-    this.state = { zip_code_value: {},
-    data:   
-    [
-        {
-        State: "",
-        LocationText: "",
-        EstimatedPopulation: {},
-        TotalWages: {},
-        }
-    ]
+    constructor(props) {
+	super(props);
+	this.state = {
+	    zip_code_value: "", //handleChange
+	    data:[] //fetchCityData
+	};
+	//IDK IF I NEED THIS STILL
+	this.handleChange = this.handleChange.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-}
+    handleChange = event => {
+	this.setState({ zip_code_value: event.target.value });
+    }
 
-handleChange(event) 
-{
-    this.setState({ zip_code_value: event.target.value });
-}
+    handleSubmit = event => {
+	event.preventDefault();
+	console.log("A Zipcode was submitted: " + this.state.zip_code_value);
+	this.fetchCityData();
+    }
 
-handleSubmit(event) {
-    this.setState({ zip_code_value: event.target.value });
-    alert('A Zipcode was submitted: ' + this.state.zip_code_value);
-    event.preventDefault();
-}
+    //After HTML from render() has finished loading, it is called once 
+    // in the component life-cycle 
+    componentDidMount() {
+    }
 
-//After HTML from render() has finished loading, it is called once 
-// in the component life-cycle 
-componentDidMount() {
-    this.fetchCityData();
-}
+    fetchCityData() {
+	axios.get("http://ctp-zip-api.herokuapp.com/zip/"+this.state.zip_code_value)
+            .then(response => {
+		console.log(response.data);
+                this.setState({data: response.data});
+            })
+	    .then( () => console.log(this.state.data))
+            .catch(error => console.log(error))
+    }
 
-fetchCityData() {
-    axios.get("http://ctp-zip-api.herokuapp.com/zip/"+this.state.zip_code_value)
-        .then(response => {
-            console.log(response.data);
-                var wanted = [this.state.zip_code_value];
-                var result = response.data.filter(zipcode =>wanted.includes(zipcode.id)  )
-                this.setState({data: result})
-        })
-        .catch(error => console.log(error));
+    displayData = () => {
+	const { data } = this.state;
+	if(data.length !== 0) {
+	    let result = data.map( zip => (
+		    <div className="zip" key={zip.RecordNumber}>{zip.Zipcode} + {zip.City}
+		</div>
+	    ));
+	    return result;
+	}
+	else {
+	    return null;
+	}
+    }
 
-}
-
-
-render() {
-    return (
-        <div>
-            <h1>Zip Code</h1>
-            <input id="zipcode" type="text" placeholder="zip code" onChange={this.handleSubmit}></input>
-       </div>
-    );
-}
+    render() {
+	return (
+		<div>
+		<h1>Zip Code</h1>
+		<form onSubmit={this.handleSubmit}>
+		<input id="zipcode" type="text" placeholder="Zip Code" onChange={this.handleChange}></input>
+		<button type="submit">Submit</button>
+		</form>
+		<div>{this.displayData()}</div>
+	    </div>
+	);
+    }
 }
 
 Zipcode.propType =
-{
-    zip_code_value: PropTypes.number
-}
+    {
+	zip_code_value: PropTypes.number
+    }
 
 export default Zipcode;
