2e7cef9226b594fe4747847bc4632800ceca8bbd
Emily Fang <fangemily60@gmail.com>
2020-01-09 13:12:08 -0500

    Skeleton of proj2 made; however, display is showing zipcodes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Zipcode from './Zipcode'
+import City from './City'
 
 class App extends Component {
   render() {
-    return (
-      <div className="zip_code_page">
-        
-      <Zipcode />
-
-      </div>
+      return (
+	      <div>
+	      <div className="zip_code_page">
+	      <Zipcode />
+	      </div>
+	      <div className="city_page">
+	      <City />
+	      </div>
+	      </div>
     );
   }
 }
